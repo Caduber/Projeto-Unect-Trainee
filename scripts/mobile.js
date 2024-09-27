f9afce03c0954a42e47a00185efe8b7a4494a0a2
Carlos Eduardo Drago <75817277+Caduber@users.noreply.github.com>
2024-09-26 22:08:06 -0300

    Add files via upload

--- scripts/mobile.js
+++ scripts/mobile.js
@@ -26,22 +26,22 @@ function avancaProxMobile(){
     
 
     if(controlemobile == 1){
-        item1.setAttribute("src", "/src/imgs/slid1.png")
+        item1.setAttribute("src", "./src/imgs/slid1.png")
         title1.innerHTML = "Brigadeiro de Capuccino"
         leg1.innerHTML = legBrigadeiro
     }
     else if (controlemobile == 2){
-        item1.setAttribute("src", "/src/imgs/slid2.png")
+        item1.setAttribute("src", "./src/imgs/slid2.png")
         title1.innerHTML = "Pão de mel"
         leg1.innerHTML = legPao
     }
     else if (controlemobile == 3){
-        item1.setAttribute("src", "/src/imgs/slid3.png")
+        item1.setAttribute("src", "./src/imgs/slid3.png")
         title1.innerHTML = "Bolo de chocolate"
         leg1.innerHTML = legBolo
     }
     else if (controlemobile == 4){
-        item1.setAttribute("src", "/src/imgs/slid4.png")
+        item1.setAttribute("src", "./src/imgs/slid4.png")
         title1.innerHTML = "Sorvete de Café"
         leg1.innerHTML = legSorvete
     }
@@ -56,22 +56,22 @@ function voltaAntMobile(){
     }
 
     if(controlemobile == 1){
-        item1.setAttribute("src", "/src/imgs/slid1.png")
+        item1.setAttribute("src", "./src/imgs/slid1.png")
         title1.innerHTML = "Brigadeiro de Capuccino"
         leg1.innerHTML = legBrigadeiro
     }
     else if (controlemobile == 2){
-        item1.setAttribute("src", "/src/imgs/slid2.png")
+        item1.setAttribute("src", "./src/imgs/slid2.png")
         title1.innerHTML = "Pão de mel"
         leg1.innerHTML = legPao
     }
     else if (controlemobile == 3){
-        item1.setAttribute("src", "/src/imgs/slid3.png")
+        item1.setAttribute("src", "./src/imgs/slid3.png")
         title1.innerHTML = "Bolo de chocolate"
         leg1.innerHTML = legBolo
     }
     else if (controlemobile == 4){
-        item1.setAttribute("src", "/src/imgs/slid4.png")
+        item1.setAttribute("src", "./src/imgs/slid4.png")
         title1.innerHTML = "Sorvete de Café"
         leg1.innerHTML = legSorvete
     }
